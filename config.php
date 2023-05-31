12ff22aa64b9043aa9ca64e57f71d482589f8801
JuliaKapica <jkapica@edu.cdv.pl>
2023-05-31 15:45:21 +0200

    Update z polaczeniem bazy danych

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$db['host'] = "admin"; // Host name
+$db['user'] = "admin"; // Mysql username
+$db['pass'] = "admin"; // Mysql password
+$db['name'] = "aplikacja_bankowa"; // Database name
+?>
